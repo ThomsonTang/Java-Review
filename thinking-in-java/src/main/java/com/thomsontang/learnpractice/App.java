744078f8b5ccf72fa7de31ab74485fa86c198d70
ThomsonTang <thomsontang@yeah.net>
2012-09-10 16:09:31 +0800

    add the thing-in-java module.

--- thinking-in-java/src/main/java/com/thomsontang/learnpractice/App.java
+++ thinking-in-java/src/main/java/com/thomsontang/learnpractice/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String str = new String("abc");
+        String str1 = "abc";
+
+        System.out.println(str == str1);
+
+        String hello = "Hello唐贵科";
+        System.out.println("length:" + hello.length());
+        System.out.println("code point count:" + hello.codePointCount(0, hello.length()));
     }
 }
