8b0f76fb58f9a0a3908077a4b614998fdcdc3c93
ThomsonTang <thomsontang@yeah.net>
2013-06-18 19:33:59 +0800

    test controller

--- junit-in-action/src/test/java/ch03/mastering/TestDefaultController.java
+++ junit-in-action/src/test/java/ch03/mastering/TestDefaultController.java
@@ -3,6 +3,8 @@ package ch03.mastering;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertSame;
+
 /**
  * Test-Case for default controller.
  *
@@ -19,8 +21,36 @@ public class TestDefaultController {
         defaultController = new DefaultController();
     }
 
-    @Test
     public void testMethod() {
         throw new RuntimeException("implement me");
+    }
+
+    private class SampleRequest implements Request {
+
+        @Override
+        public String getName() {
+            return "TEST";
+        }
+    }
+
+    private class SampleHandler implements RequestHandler {
+
+        @Override
+        public Response process(Request request) throws Exception {
+            return new SampleResponse();
+        }
+    }
+
+    private class SampleResponse implements Response {
+        //empty the response
+    }
+
+    @Test
+    public void testAddHandler() {
+        Request request = new SampleRequest();
+        RequestHandler requestHandler = new SampleHandler();
+        defaultController.addHandler(request, requestHandler);
+        RequestHandler requestHandler2 = defaultController.getHandler(request);
+        assertSame("Handler was set in controller should be the same handler we get", requestHandler, requestHandler2);
     }
 }
