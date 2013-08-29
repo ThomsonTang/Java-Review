3c6b1f9c24909e77961fff6f5e52bc8775623dea
Thomson Tang <guiketang@gmail.com>
2013-08-29 09:04:02 +0800

    commit modified

---
+++ effective-java/src/main/java/effective/enumsandannotations/item31/Ensemble.java
@@ -0,0 +1,11 @@
+package effective.enumsandannotations.item31;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author Thomson Tang
+ * @version 1.0-SNAPSHOT
+ * @date 8/26/13
+ */
+public enum Ensemble {
+}
