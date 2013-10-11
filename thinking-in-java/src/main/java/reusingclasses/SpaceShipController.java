97dabe89eb978d096bffeafcbd095d386fe26932
Thomson Tang <thomsontang@yeah.net>
2013-10-11 22:45:17 +0800

    reusing classes.

---
+++ thinking-in-java/src/main/java/reusingclasses/SpaceShipController.java
@@ -0,0 +1,12 @@
+package reusingclasses;
+
+/**
+ *  Reusing Classes/Delegation
+ *
+ * @author Thomson Tang
+ * @date 10/11/13
+ */
+public class SpaceShipController {
+    void up(int velocity) {}
+    void down(int veloctiy) {}
+}
---
+++ thinking-in-java/src/main/java/reusingclasses/SpaceShipDelegation.java
@@ -0,0 +1,32 @@
+package reusingclasses;
+
+/**
+ * Reusing Classes/Delegation
+ * <p/>
+ * It's more accurate to say that a SpaceShip contains SpaceShipControllers,
+ * and at the same time all the methods in SpaceShipControllers are exposed
+ * in a SpaceShip.
+ * ----------------------Delegation solve this dilemma.
+ * <p/>
+ * caution: Java language doesn't support delegation directly.
+ *
+ * @author Thomson Tang
+ * @date 10/11/13
+ */
+public class SpaceShipDelegation {
+    private String name;
+    private SpaceShipController spaceShipController = new SpaceShipController();
+
+    public SpaceShipDelegation(String name) {
+        this.name = name;
+    }
+
+    //Delegated methods
+    public void up(int velocity) {
+        spaceShipController.up(velocity);
+    }
+
+    public void down(int velocity) {
+        spaceShipController.down(velocity);
+    }
+}
