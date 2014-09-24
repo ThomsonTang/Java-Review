57eabbb1066864f37ce203a426704098174cb69d
ThomsonTang <guiketang@gmail.com>
2014-09-25 00:04:12 +0800

    change the build script

---
+++ effective-java/src/main/java/effective/classesandinterfaces/item13/Test.java
@@ -0,0 +1,11 @@
+package effective.classesandinterfaces.item13;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author ThomsonTang
+ * @version ${VERSION}
+ * @date 8/1/14
+ */
+public class Test {
+}
