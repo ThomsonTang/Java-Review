9070bc24c84ea59e12a078043beaebaec3dbd1ba
ThomsonTang <guiketang@gmail.com>
2014-10-01 23:59:26 +0800

    rename the personal-pratice

--- thinking-in-java/src/main/java/annotations/Test.java
+++ thinking-in-java/src/main/java/annotations/Test.java
@@ -6,7 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Created with IntelliJ IDEA.
+ * The definition of the @Test annotation.
+ * This give us an example how to define an annotation.
+ *
+ * This is a marker-annotation which which without any elements.
  *
  * @author Thomson Tang
  * @version 1.0-SNAPSHOT
