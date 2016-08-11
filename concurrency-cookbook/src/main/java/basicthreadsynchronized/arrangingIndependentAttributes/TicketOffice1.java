2e7a4805eeed5832f905088edb34e4a4d4fb46b7
ThomsonTang <guiketang@gmail.com>
2016-08-11 17:38:19 +0800

    CM: arranging independent attributes in synchronized classes.

---
+++ concurrency-cookbook/src/main/java/basicthreadsynchronized/arrangingIndependentAttributes/TicketOffice1.java
@@ -0,0 +1,27 @@
+package basicthreadsynchronized.arrangingIndependentAttributes;
+
+/**
+ * The first ticket office.
+ *
+ * @author Thomson Tang
+ * @version Created ：2016-8/11/16-16:34
+ */
+public class TicketOffice1 implements Runnable {
+    private Cinema cinema;
+
+    public TicketOffice1(Cinema cinema) {
+        this.cinema = cinema;
+    }
+
+    @Override
+    public void run() {
+        cinema.sellTickets1(3);
+        cinema.sellTickets1(2);
+        cinema.sellTickets2(2);
+        cinema.returnTickets1(3);
+        cinema.sellTickets1(5);
+        cinema.sellTickets2(2);
+        cinema.sellTickets2(2);
+        cinema.sellTickets2(2);
+    }
+}
---
+++ concurrency-cookbook/src/main/java/basicthreadsynchronized/arrangingIndependentAttributes/TicketOffice2.java
@@ -0,0 +1,27 @@
+package basicthreadsynchronized.arrangingIndependentAttributes;
+
+/**
+ * The second ticket office.
+ *
+ * @author Thomson Tang
+ * @version Created ：2016-8/11/16-16:39
+ */
+public class TicketOffice2 implements Runnable {
+    private Cinema cinema;
+
+    public TicketOffice2(Cinema cinema) {
+        this.cinema = cinema;
+    }
+
+    @Override
+    public void run() {
+        cinema.sellTickets2(2);
+        cinema.sellTickets2(4);
+        cinema.sellTickets1(2);
+        cinema.sellTickets1(1);
+        cinema.returnTickets2(2);
+        cinema.sellTickets1(3);
+        cinema.sellTickets2(2);
+        cinema.sellTickets1(2);
+    }
+}
