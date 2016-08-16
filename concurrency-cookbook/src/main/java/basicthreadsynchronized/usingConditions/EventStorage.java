cdca56536ed53c092fdabc9001d6389085dba3a7
ThomsonTang <guiketang@gmail.com>
2016-08-16 19:07:09 +0800

    CM: using conditions in synchronized code.

---
+++ concurrency-cookbook/src/main/java/basicthreadsynchronized/usingConditions/EventStorage.java
@@ -0,0 +1,50 @@
+package basicthreadsynchronized.usingConditions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * The event storage.
+ *
+ * @author Thomson Tang
+ * @version Created ：2016-8/11/16-17:56
+ */
+public class EventStorage {
+    private static final Logger logger = LoggerFactory.getLogger(EventStorage.class);
+    private int maxSize;
+    private List<Date> storage;
+
+    public EventStorage() {
+        this.maxSize = 10;
+        this.storage = new LinkedList<>();
+    }
+
+    public synchronized void set() {
+        while (storage.size() == maxSize) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                logger.error("the error occurred.", e);
+            }
+        }
+        ((LinkedList<Date>) storage).offer(new Date());
+        logger.info("Set: {}", storage.size());
+        notifyAll();
+    }
+
+    public synchronized void get() {
+        while (storage.size() == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                logger.error("the error occurred.", e);
+            }
+        }
+        logger.info("Get: {}: {}", storage.size(), ((LinkedList<?>) storage).poll());
+        notifyAll();
+    }
+}
---
+++ concurrency-cookbook/src/main/java/basicthreadsynchronized/usingConditions/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * <h2>Using conditions in synchronized code</h2>
+ *
+ * A classic problem in concurrent programming is the <strong>producer-consumer</strong> problem.
+ *
+ * <p>For these types of situations, Java provides the {@link java.lang.Object#wait()}, {@link
+ * java.lang.Object#notify()}, {@link java.lang.Object#notifyAll()}</p> methods implemented in the
+ * {@link java.lang.Object} class. A thread can call the {@code wait()}  method inside a {@code
+ * synchronized} block of code. If it call the {@code wait()} method outside a {@code synchronized}
+ * block of code, the JVM throws an {@link java.lang.IllegalMonitorStateException} exception. When
+ * the thread call the {@code wait()} method, the JVM puts the thread to sleep and releases the object
+ * that controls the {@code synchronized} block of code that it's executing and allows the other threads
+ * to execute other blocks of {@code synchronized} code protected by that object. To wake up the thread,
+ * you must call the {@code notify()} or {@code notifyAll()} method inside a block of code protected
+ * by the same object.
+ *
+ * @author Thomson Tang
+ * @version Created ：2016-8/11/16-17:39
+ */
+package basicthreadsynchronized.usingConditions;
