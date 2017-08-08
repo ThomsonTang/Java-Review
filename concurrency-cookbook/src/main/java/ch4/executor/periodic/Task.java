fe896e28eb1593838b6cfb99e7b7f4d39186446f
ThomsonTang <guiketang@gmail.com>
2017-08-08 22:16:29 +0800

    DEV-CM: running a task in an executor periodically.

---
+++ concurrency-cookbook/src/main/java/ch4/executor/periodic/Task.java
@@ -0,0 +1,23 @@
+package ch4.executor.periodic;
+
+import java.util.Date;
+import java.util.concurrent.Callable;
+
+/**
+ * A task class which implements the {@link Runnable} interface.
+ *
+ * @author Thomson Tang
+ * @version Created: 05/08/2017.
+ */
+public class Task implements Runnable {
+    private String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s: starting at: %s\n", name, new Date());
+    }
+}
