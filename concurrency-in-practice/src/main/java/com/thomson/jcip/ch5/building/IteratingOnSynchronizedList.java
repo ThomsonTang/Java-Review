877dcfc279a8f2847a8d57c6c176f3e3cb8b17e7
ThomsonTang <guiketang@gmail.com>
2017-08-15 10:25:11 +0800

    JCIP: Current modification exception was threw while iterating an synchronized collection.

--- concurrency-in-practice/src/main/java/com/thomson/jcip/ch5/building/IteratingOnSynchronizedList.java
+++ concurrency-in-practice/src/main/java/com/thomson/jcip/ch5/building/IteratingOnSynchronizedList.java
@@ -10,6 +10,10 @@ import java.util.List;
  * 虽然我们创建的list是一个同步集合类对象，但使用迭代器进行遍历的时候，可能会抛出{@link java.util.ConcurrentModificationException}异常。
  * 这是因为在遍历的过程中，有可能有别的线程也在修改该集合对象。
  *
+ * 相应的解决方法有两种：
+ * 1. 对集合类加同步锁
+ * 2. 遍历的过程中clone集合类对象，并迭代clone对象
+ *
  * @author Thomson Tang
  * @version Created: 14/08/2017.
  */
