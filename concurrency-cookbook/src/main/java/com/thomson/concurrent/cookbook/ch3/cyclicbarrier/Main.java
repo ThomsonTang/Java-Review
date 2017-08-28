3466e92c6f9c4e455913cc4bc2251fde94b28aab
ThomsonTang <guiketang@gmail.com>
2017-08-28 23:37:22 +0800

    JCC：synchronize tasks in a common point.

--- concurrency-cookbook/src/main/java/com/thomson/concurrent/cookbook/ch3/cyclicbarrier/Main.java
+++ concurrency-cookbook/src/main/java/com/thomson/concurrent/cookbook/ch3/cyclicbarrier/Main.java
@@ -1,5 +1,10 @@
 package com.thomson.concurrent.cookbook.ch3.cyclicbarrier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CyclicBarrier;
+
 /**
  * 类说明
  *
@@ -7,11 +12,28 @@ package com.thomson.concurrent.cookbook.ch3.cyclicbarrier;
  * @version Created: 28/08/2017.
  */
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         final int ROWS = 10000;
         final int NUMBERS = 1000;
         final int SEARCH = 5;
         final int PARTICIPANTS = 5;
         final int LINES_PARTICIPANT = 2000;
+
+        MatrixMock matrixMock = new MatrixMock(ROWS, NUMBERS, SEARCH);
+        Results results = new Results(ROWS);
+        Grouper grouper = new Grouper(results);
+        CyclicBarrier barrier = new CyclicBarrier(PARTICIPANTS, grouper);
+
+        Searcher searcher[] = new Searcher[PARTICIPANTS];
+        for (int i = 0; i < PARTICIPANTS; i++) {
+            searcher[i] = new Searcher(i * LINES_PARTICIPANT, (i * LINES_PARTICIPANT) + LINES_PARTICIPANT, matrixMock, results, 5, barrier);
+            Thread thread = new Thread(searcher[i]);
+            thread.start();
+        }
+
+        LOGGER.info("Main: The main thread has finished!");
+
     }
 }
