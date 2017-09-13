ed61e3296919ca3104d3e688736013e85040ac7f
ThomsonTang <guiketang@gmail.com>
2017-09-13 13:48:25 +0800

    JCC: executing tasks in an executor that returns a result.

---
+++ concurrency-cookbook/src/main/java/com/thomson/concurrent/cookbook/ch4/future/Main.java
@@ -0,0 +1,10 @@
+package com.thomson.concurrent.cookbook.ch4.future;
+
+/**
+ * <h2>Executing tasks in an executor that returns a result</h2>
+ *
+ * @author Thomson Tang
+ * @version Created: 13/09/2017.
+ */
+public class Main {
+}
